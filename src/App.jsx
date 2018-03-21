3f1dc240fe043e7fc55a7d5efa8e74e634727ed5
borodkoleg <borodkoleg@gmail.com>
2018-03-21 16:23:56 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FacebookBtn from './FacebookBtn.jsx';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FacebookBtn/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/FacebookBtn.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FacebookBtn extends Component {
+  render() {
+    return (
+      <div className="test">
+         Test
+      </div>
+    );
+  }
+}
+
+export default FacebookBtn;
