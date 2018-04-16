539c16faf682e44bce8e000cd66d64fd048e80f3
borodkoleg <borodkoleg@gmail.com>
2018-04-16 20:23:46 +0300

    user add, user remove

---
+++ src/components/UserAdd.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+var axios = require('axios');
+
+class UserAdd extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+    //
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    // if (cookies.get('market_admin_co')){
+    //   this.props.history.push('/admin');
+    // }
+    // if (cookies.get('market_user_co')){
+    //   this.props.history.push('/facebook');
+    // }
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    var this_=this;
+
+    axios({
+      url: 'https://'+process.env.HOST_RAILS+'/api/admin/user_add',
+      method: 'post',
+      headers: {'Token': this_.props.g_token['token']},
+      data: {
+        email: this_.refs.email_input.value,
+        password: this_.refs.password_input.value,
+        name: this_.refs.name_input.value
+      }
+    })
+    .then(function (response) {
+      if (response){
+        if (response['data']){
+          this_.props.user_add(response['data']['user']);
+        }
+      }
+    })
+    .catch(function (response) {
+      if (response.response){
+        this_.setState({
+          error: response.response.data.error
+        });
+      }else{
+        this_.setState({
+          error: 'Server connection error'
+        });
+      }
+    });
+  }
+
+  render() {
+    const {error} = this.state;
+
+    return (
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          <li>
+            <input
+            type="text"
+            placeholder="E-mail"
+            ref="email_input"
+            />
+          </li>
+          <li>
+            <input
+            type="password"
+            placeholder="Password"
+            ref="password_input"
+            />
+          </li>
+          <li>
+            <input
+            type="text"
+            placeholder="name"
+            ref="name_input"
+            />
+          </li>
+          <li>
+            <button>User add</button>
+          </li>
+        </ul>
+      </form>
+      {error}
+    </div>
+    );
+  }
+}
+
+export default connect(
+state => ({
+  g_token: state.currentUser
+  // g_users: state.users,
+  // g_campaigns: state.campaigns,
+  // g_ads: state.ads
+}),
+dispatch => ({
+  // g_emailChange: (email) => {
+  //   dispatch({ type: 'CURRENT_EMAIL_CHANGE', payload: email });
+  // },
+})
+)(UserAdd);
--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Logout from '../components/Logout';
+import UserAdd from '../components/UserAdd';
 import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
@@ -16,6 +17,8 @@ class Admin extends Component {
 
     this.users_show = this.users_show.bind(this);
     this.error = this.error.bind(this);
+    this.user_add = this.user_add.bind(this);
+    this.user_delete = this.user_delete.bind(this);
   }
 
   error(){
@@ -23,8 +26,68 @@ class Admin extends Component {
     window.location = '/';
   }
 
-  users_show(){
+  user_delete(email, index){
+    var this_=this;
+    axios({
+      url: 'https://'+process.env.HOST_RAILS+'/api/admin/user_delete',
+      method: 'delete',
+      headers: {'Token': this_.props.g_currentUser['token']},
+      data: {
+        email: email,
+      }
+    })
+    .then(function (response) {
+      if (response){
+        if (response['data']){
+          let users = this_.state.users;
+          users['users'].splice(index, 1);
+          this_.setState({
+            users: users
+          });
+        }
+      }
+    })
+    .catch(function (response) {
+      if (response.response){
+        // this_.setState({
+        //   error: response.response.data.error
+        // });
+      }else{
+        // this_.setState({
+        //   error: 'Server connection error'
+        // });
+      }
+    });
+  }
 
+  user_add(user){
+     let users = this.state.users;
+     users['users'].push(user);
+     this.setState({
+       users: users
+     });
+  }
+
+  users_show(token){
+    var this_=this;
+    axios({
+      url: 'https://'+process.env.HOST_RAILS+'/api/admin/users',
+      method: 'post',
+      headers: {'Token': token}
+    }).then(function (response) {
+      if (response){
+        if (response['data']){
+          //if (response['data'].isArray) {
+            this_.setState({users: response['data']});
+          //}
+          return;
+        }
+      }
+      this_.error();
+    })
+    .catch(function (response) {
+      this_.error();
+    });
   }
 
   componentDidMount(){
@@ -39,13 +102,13 @@ class Admin extends Component {
       response_params['headers'] = {'Token': this_.props.g_currentUser['token']};
     }
 
-    axios(response_params)
-    .then(function (response) {
+    axios(response_params).then(function (response) {
       if (response){
         if (response['data']){
           if (response['data']['email'] === process.env.ADMIN_EMAIL) {
             this_.props.g_tokenChange(response['data']['token']);
             cookies.set('market_admin_co', response['data']['token'], { path: '/', secure: true });
+            this_.users_show(response['data']['token']);
             return;
           }
         }
@@ -58,11 +121,21 @@ class Admin extends Component {
   }
 
   render() {
-    //  const {campaignsGlobal, initialApi, usersGlobal} = this.props;
+    //const {user_add} = this.props;
+    const {users} = this.state;
     return (
     <div>
       <Logout facebook_logout={false}/>
       <br/><br/>
+      <UserAdd user_add={this.user_add}/>
+      <br/><br/>
+      {users['users'] && users['users'].map((key, idx) => {
+        return (
+        <div key={idx}>
+          {key['email']}  {key['name']}  <div onClick={() => this.user_delete(key['email'], idx)}>Delete</div>
+        </div>
+        );
+      })}
     </div>
     );
   }
