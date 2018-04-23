46eba3cf2bdfa5d788994d04b6481fd9340329e1
borodkoleg <borodkoleg@gmail.com>
2018-04-23 22:09:01 +0300

    graphics functional changed

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-var LineChart = require("react-chartjs").Line;
+import {Line} from 'react-chartjs-2';
 // import Cookies from 'universal-cookie';
 // var axios = require('axios');
 // const cookies = new Cookies();
@@ -23,55 +23,53 @@ class Chart extends Component {
   render() {
     const {g_chart} = this.props;
     // const {selectValue} = this.state;
+    var spend_max = Math.max(...g_chart.spends_array)*3;
+    var click_max = Math.max(...g_chart.clicks_array)*5;
+    var impression_max = Math.max(...g_chart.impressions_array)*1.2;
+    var action_max = Math.max(...g_chart.actions_array)*8;
+
     var datasets_now = [];
     datasets_now.push(
     {
       label: "Cost",
       yAxisID: 'Cost',
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
       data: g_chart.spends_array,
+      borderColor: 'rgba(92,161,71,1)',
+      pointRadius: 5,
+      pointHoverRadius: 10,
+      backgroundColor: 'transparent'
     },
     {
       label: "Clicks",
       yAxisID: 'Clicks',
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: g_chart.clicks_array
+      data: g_chart.clicks_array,
+      borderColor: 'rgba(255,78,39,1)',
+      pointRadius: 5,
+      pointHoverRadius: 10,
+      backgroundColor: 'transparent'
     },
     {
       label: "Impressions",
       yAxisID: 'Impressions',
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: g_chart.impressions_array
+      data: g_chart.impressions_array,
+      borderColor: 'rgba(255,17,134,1)',
+      pointRadius: 5,
+      pointHoverRadius: 10,
+      backgroundColor: 'transparent'
     },
     {
       label: "Actions",
       yAxisID: 'Actions',
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: g_chart.actions_array
+      data: g_chart.actions_array,
+      borderColor: 'rgba(25,125,255,1)',
+      pointRadius: 5,
+      pointHoverRadius: 10,
+      backgroundColor: 'transparent'
     },
     );
 
     var chartData = {
+      type: 'line',
       labels: g_chart.days,
       datasets: datasets_now,
     };
@@ -82,56 +80,62 @@ class Chart extends Component {
       //fill: false,
       //showLine: false,
       responsive: true,
-      //legend: false,
-      //options.scales.xAxes[0].gridLines.display = false;
-
       //scaleOverride : true,
       //scaleSteps : 10,
       //scaleStepWidth : 50,
       //scaleStartValue : 0
-      options : {
-        scales : {
-          xAxes : [ {
-            gridLines : {
-              display : false
-            }
-          } ]
-        }
-      },
 
-      // options: {
-      //   scales: {
-      //     yAxes: [{
-      //       id: 'Cost',
-      //       type: 'linear',
-      //       position: 'left',
-      //     }, {
-      //       id: 'Clicks',
-      //       type: 'linear',
-      //       position: 'left',
-      //       ticks: {
-      //         max: 1,
-      //         min: 0,
-      //         stepWidth: 2
-      //       }
-      //     },
-      //       {
-      //         id: 'Impressions',
-      //         type: 'linear',
-      //         position: 'left',
-      //       },
-      //       {
-      //         id: 'Actions',
-      //         type: 'linear',
-      //         position: 'left',
-      //       }]
-      //   }
-      // }
+      tooltips: {
+        mode: 'index',
+        intersect: false
+      },
+      scales: {
+        yAxes: [{
+          type: 'linear',
+          position: 'left',
+          id: 'Cost',
+          ticks: {
+            max: spend_max,
+            min: 0
+          },
+          stacked: true
+        }, {
+          type: 'linear',
+          position: 'left',
+          id: 'Clicks',
+          ticks: {
+            max: click_max,
+            min: 0
+          },
+          display:false
+        },
+          {
+            type: 'linear',
+            position: 'left',
+            id: 'Impressions',
+            ticks: {
+              max: impression_max,
+              min: 0
+            },
+            display:false
+          },
+          {
+            type: 'linear',
+            position: 'left',
+            id: 'Actions',
+            ticks: {
+              max: action_max,
+              min: 0
+            },
+            display:false
+          }
+        ]
+      }
 
     };
     return (
     <div className="chart">
-      <LineChart data={chartData} options={chartOptions} redraw/>
+      <Line data={chartData} options={chartOptions} redraw/>
     </div>
     );
   }
